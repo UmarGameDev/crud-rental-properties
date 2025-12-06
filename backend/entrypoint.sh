e3d231401cd8ddbccf135c99763544375a9ffb29
sakeenabatool <sakeenabatool358@gmail.com>
2025-12-06 13:13:34 +0500

    ready for deployment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-# Run database migrations
+PORT=${PORT:-8000}
 poetry run alembic upgrade head
-
-# Start the FastAPI app
-poetry run uvicorn --host 0.0.0.0 --port 8000 backend.main:app
+poetry run uvicorn backend.main:app --host 0.0.0.0 --port $PORT
